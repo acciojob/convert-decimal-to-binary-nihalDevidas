3a186ad17c87551480b52d0cd07774a352a01454
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-12-07 19:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function decimalToBinary(num) {
   // your code here
+	let result = "";
+
+	while(num  > 0){
+		let d = num %  2;
+		result = d+ result;
+		num = Math.floor(num / 2);
+		
+	}
+	return result;
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
